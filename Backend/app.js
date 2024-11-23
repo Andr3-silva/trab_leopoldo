c0344f49b3be4b461049354990b92530898c5bdd
André Silva <Andr3-silva>
2024-11-23 14:30:55 -0300

    Salvando pontuação no banco

--- Backend/app.js
+++ Backend/app.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require("path");
 const cadastro = require("./routes/Cadastro/cadastro");
 const login = require("./routes/Cadastro/login");
+const score = require("./routes/score")
 
 require("../Backend/config/db");
 
@@ -12,6 +13,7 @@ app.get("/", (req, res) => {});
 
 app.use("/", cadastro);
 app.use("/", login);
+app.use("/score", score)
 
 app.listen(1000, () => {
   console.log("Servidor Rodando na porta 1000");
